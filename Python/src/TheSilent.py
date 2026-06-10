3d97a08df83197c9aeb4d944249981d4e8f16c53
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-06-09 22:16:22 -0500

    Add files via upload

---
+++ Python/src/TheSilent.py
@@ -0,0 +1,16 @@
+import subprocess
+import sys
+import threading
+
+class Threading:
+    def __init__(self, func):
+        self.func = func
+    def __call__(self, *args, **kwargs):
+        threading.Thread(target=self.func, args=args, kwargs=kwargs).start()
+
+if __name__ == "__main__":
+    try:
+        subprocess.Popen(sys.argv[1:])
+    
+    except IndexError:
+        pass
